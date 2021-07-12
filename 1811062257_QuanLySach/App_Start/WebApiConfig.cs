9e970aa89d7f65fb7d5e0559c64b737189474cbe
THANH QUANG <THANH QUANG@DESKTOP-0G74NBM>
2021-07-12 13:32:24 +0700

    còn bị lỗi chỗ router

--- 1811062257_QuanLySach/App_Start/WebApiConfig.cs
+++ 1811062257_QuanLySach/App_Start/WebApiConfig.cs
@@ -9,16 +9,15 @@ namespace _1811062257_QuanLySach
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new { id = RouteParameter.Optional }        
             );
+         
+
         }
     }
 }
